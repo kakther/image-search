5b09492d11b61f9fb53b54c8dd4cbda626457340
Kausar Akther <kausar.akther@gmail.com>
2020-05-31 02:12:46 -0400

    Updates

---
+++ precache-manifest.3cc3f22f0f66a2eae643f40aa6ed0557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277208ff5123052425a577943f3c7f95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78911c3a5de5fafda390",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f7dbb8d3196e1d26a315",
+    "url": "/static/js/2.d17480d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d17480d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78911c3a5de5fafda390",
+    "url": "/static/js/main.edfe9a2f.chunk.js"
+  },
+  {
+    "revision": "17c2657aaa962857947d",
+    "url": "/static/js/runtime-main.35e13b79.js"
+  }
+]);
